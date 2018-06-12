3e6d1f4ab78487368bcc4bd557ee380a6f6f12b9
Tejas <tejaschaudhary@hotmail.com>
2018-06-11 18:54:05 -0700

    first commit

---
+++ ex1.py
@@ -0,0 +1,23 @@
+print("Hello World")
+print("Hello"+" World")
+
+var = 'Hello World'
+
+print(var[:5])
+print(var[6:])
+
+var1 = 'Hello'*5
+lst = [i for i in range(10)]
+print(lst)
+
+lst1 = lst + lst
+print(lst1)
+
+lst_squares =list(map(lambda x: x**2, lst))
+print(lst_squares)
+
+def cube(n):
+	return n*n*n
+
+lst_cube = list(map(cube, lst))
+print(lst_cube)
